ef016746ba5cfb78ccc1a9d1e83f626c768c0680
davidjacobo <jguillen@cimat.mx>
2016-01-25 14:31:37 -0600

    Vectorize whenever possible

--- Tutorial/change_intensity_levels.m
+++ Tutorial/change_intensity_levels.m
@@ -3,12 +3,7 @@ max_intensity = max(in(:)) +1;
 lev = 2;
 quant = (max_intensity / lev);
 
-[row col] = size(in);
-
-for i=1:row
-    for j=1:col
-        in(i,j) = floor(in(i,j) / quant);
-    end
-end
+% Vectorize baby!!
+in = floor(in./quant)*quant;
 
 imshow(in);
