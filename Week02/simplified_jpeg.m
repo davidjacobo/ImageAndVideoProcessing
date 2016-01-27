6145fa7936da01258ca1401b79a37c833dc83da0
davidjacobo <jguillen@cimat.mx>
2016-01-27 16:33:52 -0600

    Simplified JPEG
    Simplifying some of the operations, also fixing a couple of bugs related
    to the nxn subsquares and making the output a little easier to see: side
    to side, original input - transformed and quantized image - output

--- Week02/simplified_jpeg.m
+++ Week02/simplified_jpeg.m
@@ -4,18 +4,19 @@ in = imread('lena512.bmp');
 trans = zeros(row, col);
 out = zeros(row, col);
 
+% define the side of the sub-square nxn 
+n = 8;
+
 % map the image using the DCT transform over 8x8 sub-squares
-for i=1:8:row-8
-    for j=1:8:col-8
-        sub_square = double(in(i:i+8 ,j:j+8));
-        dct_square = dct2(sub_square);
-        trans(i:i+8, j:j+8) = dct_square;
+ for i=1:n:row-n+1
+    for j=1:n:col-n+1
+         trans(i:i+n-1, j:j+n-1) = dct2(in(i:i+n-1, j:j+n-1));
     end
-end
+ end
 
 % after mapping, we can quantize in the transformation space
 % will use a value 'q' to establish that
-q = 32;
+q = 16;
 trans = floor(trans./q)*q;
 
 % the inverse process involves:
@@ -23,19 +24,14 @@ trans = floor(trans./q)*q;
 % we generated on the forward step
 
 % Reverse the transform, for either DCT or FFT the function is
-% exactly the same
-for i=1:8:row-8
-    for j=1:8:col-8
-        sub_square = trans(i:i+8 ,j:j+8);
-        dct_square = idct2(sub_square);
-        out(i:i+8, j:j+8) = dct_square;
+% exactly the same (I mean in terms of how long the script is)
+for i=1:n:row-n+1
+    for j=1:n:col-n+1
+        out(i:i+n-1, j:j+n-1) = idct2(trans(i:i+n-1, j:j+n-1));
     end
 end
 
-min_p = min(out(:));
-max_p = max(out(:));
-
-out = uint8(256*((out - min_p)./(max_p-min_p)));
-
 clf
-imshow(out);
+subplot(1,3,1); imshow(in);
+subplot(1,3,2); imshow(trans);
+subplot(1,3,3); imshow(out, [0 255]);
